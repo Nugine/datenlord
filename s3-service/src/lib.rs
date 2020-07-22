3b56a5936d2a13b555d35c3f83355a62f5643d57
Nugine <nugine@foxmail.com>
2020-07-22 23:04:14 +0800

    s3-service: impl compat

--- s3-service/src/lib.rs
+++ s3-service/src/lib.rs
@@ -1,7 +1,4 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![forbid(unsafe_code)]
+#![deny(missing_debug_implementations)]
+
+pub mod compat;
